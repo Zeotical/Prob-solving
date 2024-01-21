2f57c3a8a63000e9c3c68007f1b38238fe007027
Basmala Mohammed Ali <1231102801@student.mmu.edu.my>
2024-01-21 18:47:21 +0800

    board 5 all functions COMPLETED

--- basmala.py
+++ basmala.py
@@ -33,7 +33,7 @@ def print_board(board):
 
 def quit(user_input):
  if user_input == "q":
-     print("Thanks for playing")
+     print("Thanks for playing!")
      return True
  else: return False
  
@@ -150,92 +150,192 @@ Basmala 1231102801 code
 #check if user enters smth else make them go again
 
 """
-board4= [
-    ["-","-","-","-"],
-    ["-","-","-","-"],
-    ["-","-","-","-"],
-    ["-","-","-","-"]
+# board4= [
+#     ["-","-","-","-"],
+#     ["-","-","-","-"],
+#     ["-","-","-","-"],
+#     ["-","-","-","-"]
+# ]
+
+# def check_input4(user_input):
+#     if not isnum(user_input): return False
+#     user_input= int(user_input)
+#     if not bounds4(user_input):return False
+#     return True    
+        
+# def bounds4(user_input):
+#     if user_input<1 or user_input>16:
+#         print("This number is out of bounds")
+#         return False
+#     else: return True
+
+# def coordinates4(user_input):
+#     row= int(user_input/4)
+#     col= user_input
+#     if col>3 : col= int(col%4)
+#     return (row,col)
+
+# def check_row4(user,board4):
+#    for row in board4:
+#       complete_row = True
+#       for slot in row:
+#          if slot!= user:
+#             complete_row= False
+#             break
+#       if complete_row : return True  
+#    return False   
+   
+# def check_col4(user,board4):
+#    for col in range(4):
+#       complete_col = True
+#       for row in range(4):
+#          if board4[row][col] != user:
+#             complete_col= False
+#             break
+#       if complete_col: return True
+#    return False     
+
+# def check_diag4(user,board4):
+#    if board4[0][0] == user and board4[1][1]== user and\
+#       board4[2][2]== user and board4[3][3]== user:
+#       return True
+#    elif board4[0][3] == user and board4[1][2]== user and\
+#         board4[2][1]== user and board4[3][0]==user:
+#       return True
+#    else: return False
+
+# def iswin4(user,board4):
+#  if check_row4(user,board4): return True
+#  if check_col4(user,board4): return True
+#  if check_diag4(user,board4):return True
+   
+# turns=0
+# while turns<16:
+#     active_user= current_user(user)
+#     print_board(board4)
+#     user_input= input("Please enter a position 1 through 16 or enter \"q\" to quit: ")
+#     if quit(user_input): break
+#     if not check_input4(user_input):
+#         print("Please try again.")
+#         continue
+#     user_input= int(user_input)-1
+#     coords= coordinates4(user_input)
+#     if istaken(coords,board4): 
+#         print("Please try again.")
+#         continue
+#     add_to_board(coords,board4)
+#     if iswin4(active_user,board4):
+#        print()
+#        print_board(board4)
+#        print()
+#        print(f"{active_user} won!")
+#        break
+#     turns+=1
+#     if turns==16:
+#      print()
+#      print_board(board4)
+#      print()
+#      print("It's a draw!")
+#      break
+#     user=not user
+    
+"""
+Basmala 1231102801 code
+
+#5*5 board
+#win function
+#draw function
+#user input and toggle b/w users
+#check if space is already taken
+#check if user enters smth else make them go again
+
+"""   
+
+board5=[
+   ["-","-","-","-","-"],
+   ["-","-","-","-","-"],
+   ["-","-","-","-","-"],
+   ["-","-","-","-","-"],
+   ["-","-","-","-","-"]
 ]
 
-def check_input4(user_input):
-    if not isnum(user_input): return False
-    user_input= int(user_input)
-    if not bounds4(user_input):return False
-    return True    
-        
-def bounds4(user_input):
-    if user_input<1 or user_input>16:
-        print("This number is out of bounds")
-        return False
-    else: return True
+def  check_input5(user_input):
+   if not isnum(user_input): return False
+   user_input= int(user_input)
+   if not bounds5(user_input): return False
+   return True
+            
+def bounds5(user_input):
+   if user_input<1 or user_input>25:
+      print("This number is out of bounds.")
+      return False
+   else: return True
 
-def coordinates4(user_input):
-    row= int(user_input/4)
-    col= user_input
-    if col>3 : col= int(col%4)
-    return (row,col)
+def coordinates5(user_input):
+   row = int(user_input/5)
+   col= int(user_input)
+   if col>4: col= int(user_input%5)
+   return (row,col)
 
-def check_row4(user,board4):
-   for row in board4:
-      complete_row = True
+
+def check_row5(user,board5):
+   for row in board5:
+      complete_row=True
       for slot in row:
          if slot!= user:
             complete_row= False
             break
-      if complete_row : return True  
-   return False   
-   
-def check_col4(user,board4):
-   for col in range(4):
-      complete_col = True
-      for row in range(4):
-         if board4[row][col] != user:
+      if complete_row: return True
+   return False
+
+def check_col5(user,board5):
+   for col in range(5):
+      complete_col= True
+      for row in range(5):
+         if board5[row][col] != user:
             complete_col= False
             break
       if complete_col: return True
-   return False     
+   return False
 
-def check_diag4(user,board4):
-   if board4[0][0] == user and board4[1][1]== user and\
-      board4[2][2]== user and board4[3][3]== user:
-      return True
-   elif board4[0][3] == user and board4[1][2]== user and\
-        board4[2][1]== user and board4[3][0]==user:
-      return True
-   else: return False
+def check_diag5(user,board5):
+     if board5[0][0]== user and board5[1][1]== user\
+     and board5[2][2]==user and board5[3][3]==user\
+     and board5[4][4]==user: return True  
+     elif board5[0][4]== user and board5[1][3]== user\
+     and board5[2][2]==user and board5[3][1]==user\
+     and board5[4][0]==user: return True            
 
-def iswin4(user,board4):
- if check_row4(user,board4): return True
- if check_col4(user,board4): return True
- if check_diag4(user,board4):return True
-   
+def iswin5(user,board5):
+   if check_row5(user,board5): return True
+   if check_col5(user,board5): return True     
+   if check_diag5(user,board5): return True
 turns=0
-while turns<16:
-    active_user= current_user(user)
-    print_board(board4)
-    user_input= input("Please enter a position 1 through 16 or enter \"q\" to quit: ")
-    if quit(user_input): break
-    if not check_input4(user_input):
-        print("Please try again.")
-        continue
-    user_input= int(user_input)-1
-    coords= coordinates4(user_input)
-    if istaken(coords,board4): 
-        print("Please try again.")
-        continue
-    add_to_board(coords,board4)
-    if iswin4(active_user,board4):
-       print()
-       print_board(board4)
-       print()
-       print(f"{active_user} won!")
-       break
-    turns+=1
-    if turns==16:
-     print()
-     print_board(board4)
-     print()
-     print("It's a draw!")
-     break
-    user=not user
-    
+while turns<25:
+   print_board(board5)
+   active_user= current_user(user)
+   user_input=input("Please enter a position 1 through 25 or press \"q\" to quit: ")
+   if quit(user_input): break
+   if not check_input5(user_input):
+      print("Please try again.")
+      continue
+   user_input= int(user_input)-1
+   coords= coordinates5(user_input)
+   if istaken(coords,board5):
+      print("Please try again.")
+      continue
+   add_to_board(coords,board5)
+   if iswin5(active_user,board5):
+      print()
+      print_board(board5)
+      print()
+      print(f"{active_user} won!")
+      break
+   turns+=1
+   if turns==25:
+      print()
+      print_board(board5)
+      print()
+      print("It's a draw!")
+      break
+   user = not user
