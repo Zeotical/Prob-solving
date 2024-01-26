5bb2eefbf2cf82dbe28f0732973765f6dc5f1dad
Zeotical <151489881+Zeotical@users.noreply.github.com>
2024-01-27 07:25:53 +0800

    Update main.py changed the 5x5 board to become like the others game works well. if 5x5 doesn't work still then there must be an indentation error otherwise it is fine.

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 # Course: PSP0101 PROBLEM SOLVING AND PROGRAM DESIGN
 # Class: TL11-09
 # Year: 2023/24 Trimester 1
-# Names: Basmala Mohammed Ali Elimam | MEMBER_NAME_2 | NURUL HAIFA ALIAH BINTI MOHD YAZID
-# IDs: 1231102801 | MEMBER_ID_2 | 1231102000
+# Names: Basmala Mohammed Ali Elimam | Nur Dina Ainulhayat Binti Mohd Affandy | NURUL HAIFA ALIAH BINTI MOHD YAZID
+# IDs: 1231102801 | 123 | 1231102000
 # Emails: 1231102801@student.mmu.edu.my | MEMBER_EMAIL_2 | 1231102000@STUDENT.MMU.EDU.MY
-# Phones: MEMBER_PHONE_1 | MEMBER_PHONE_2 | 012-721-1586
+# Phones:  | MEMBER_PHONE_2 | 012-721-1586
 # *********************************************************
 
 
@@ -46,6 +46,7 @@ choice= input("Do you want to play against another player or the computer? ").lo
 if choice=="computer":
    username1 = input("Player 1 Enter your username:" )
    print ("Hello "+username1)
+   username2="computer"	
 elif choice=="player":
    username1 = input("Player 1 Enter your username:" )
    print ("Hello "+username1)
@@ -86,6 +87,7 @@ if gridsize=="1":
              if choice=="computer":
                 username1 = input("Player 1 Enter your username:" )
                 print ("Hello "+username1)
+				username2="computer" 
              elif choice=="player":
                 username1 = input("Player 1 Enter your username:" )
                 print ("Hello "+username1)
@@ -95,11 +97,11 @@ if gridsize=="1":
         ["-", "-","-"],
         ["-", "-","-"],
         ["-", "-","-"]]
+        fun.print_board(board)    
         if choice=="computer" and user== False:
             user_input= str(fun.computer(gridsize))
             print(f"The computer enters: {user_input}")
         else:
-            fun.print_board(board)
             user_input=input("Please enter a position 1 through 9 or press \"q\" to quit: ") 
         if fun.quit(user_input): break
         if not fun.check_input(user_input,gridsize):
@@ -157,21 +159,22 @@ if gridsize=="2":
              if choice=="computer":
                 username1 = input("Player 1 Enter your username:" )
                 print ("Hello "+username1)
+				username2="computer" 
              elif choice=="player":
                 username1 = input("Player 1 Enter your username:" )
                 print ("Hello "+username1)
                 username2 = input("Player 2 Enter your username:" )
                 print ("Hello "+username2)
-                board4= [         #added board since it used to take the previous games' input
+             board4= [         #added board since it used to take the previous games' input
         ["-","-","-","-"],
         ["-","-","-","-"],
         ["-","-","-","-"],
         ["-","-","-","-"]]
+        fun.print_board(board4)         
         if choice=="computer" and user== False:
             user_input= str(fun.computer(gridsize))
             print(f"The computer enters: {user_input}")
         else:
-            fun.print_board(board4)
             user_input=input("Please enter a position 1 through 16 or press \"q\" to quit: ") 
         if fun.quit(user_input): break
         if not fun.check_input(user_input,gridsize):
@@ -210,8 +213,6 @@ if gridsize=="2":
             if fun.quit(user_input): break
         user = not user
     
-  
-
 board5=[
    ["-","-","-","-","-"],
    ["-","-","-","-","-"],
@@ -220,48 +221,70 @@ board5=[
    ["-","-","-","-","-"]
 ]
 
-turns=0
 if gridsize=="3":
-    while True:
-        active_user= fun.current_user(user) 
-        if user_input=="s":
-            turns=0
-            board5=[             #added board since it used to take the previous games' input
-        ["-","-","-","-","-"], 
-        ["-","-","-","-","-"],
-        ["-","-","-","-","-"],
-        ["-","-","-","-","-"],
-        ["-","-","-","-","-"] ]         
-            fun.startagain(user_input,gridsize)
-        if choice=="computer" and user== False:
-            user_input= str(fun.computer(gridsize))
-            print(f"The computer enters: {user_input}")
-        else:
-            user_input=input("Please enter a position 1 through 9 or press \"q\" to quit: ")   
-        if fun.startagain(user_input):continue
+  while True:
+      if user_input=="s":
+           turns=0
+           fun.startagain(user_input,gridsize)
+           choice= input("Do you want to play against another player or the computer? ").lower()
+           gridsize="3"
+           fun.tic_tac_toe(gridsize)
+           if choice=="computer":
+              username1 = input("Player 1 Enter your username:" )
+              print ("Hello "+username1)
+              username2="computer"
+           elif choice=="player":
+              username1 = input("Player 1 Enter your username:" )
+              print ("Hello "+username1)
+              username2 = input("Player 2 Enter your username:" )
+              print ("Hello "+username2)
+
+           board5=[            
+              ["-","-","-","-","-"], 
+              ["-","-","-","-","-"],
+              ["-","-","-","-","-"],
+              ["-","-","-","-","-"],
+              ["-","-","-","-","-"] ]   
+      fun.print_board(board5)
+      if choice=="computer" and user== False:
+           user_input= str(fun.computer(gridsize))
+           print(f"The computer enters: {user_input}")
+      else:
+          user_input=input("Please enter a position 1 through 9 or press \"q\" to quit: ").lower()  
+      if fun.quit(user_input): break
+      if not fun.check_input(user_input,gridsize):
+          print("Please try again.")
+          continue
+      user_input= int(user_input)-1
+      active_user = fun.current_user(user)
+      coords= fun.coordinates5(user_input)
+      if fun.istaken(coords,board5):
+          print("Please try again.")
+          continue
+      fun.add_to_board(coords,board5,active_user)
+      if user and fun.iswin5(active_user,board5):
+         print()
+         fun.print_board(board5)
+         print()
+         user=username1 
+         print(f"{username1} won!")
+         user_input=input("To play again enter \"s\", to quit enter \"q\": ").lower() 
+         if fun.quit(user_input): break
+      elif user==False and fun.iswin5(active_user,board5):
+        print()
+        fun.print_board(board5)
+        print()
+        user=username2
+        print(f"{username2} won!")
+        user_input=input("To play again enter \"s\", to quit enter \"q\": ").lower() 
         if fun.quit(user_input): break
-        if not fun.check_input(user_input,gridsize):
-            print("Please try again.")
-            continue
-        user_input= int(user_input)-1
-        coords= fun.coordinates5(user_input)
-        if fun.istaken(coords,board5):
-            print("Please try again.")
-            continue
-        fun.add_to_board(coords,board5,active_user)
-        if fun.iswin5(active_user,board5):
-            print()
-            fun.print_board(board5)
-            print()
-            print(f"{active_user} won!")
-            user_input=input("To play again enter \"s\", to quit enter \"q\": ").lower() 
-        if fun.quit(user_input): break
+
         turns+=1
         if turns==25:
-            print()
-            fun.print_board(board5)
-            print()
-            print("It's a draw!")
-            user_input=input("To play again enter \"s\", to quit enter \"q\": ").lower() 
-        if fun.quit(user_input): break
-        user = not user
+          print()
+          fun.print_board(board5)
+          print()
+          print("It's a draw!")
+          user_input=input("To play again enter \"s\", to quit enter \"q\": ").lower() 
+          if fun.quit(user_input): break
+      user = not user
